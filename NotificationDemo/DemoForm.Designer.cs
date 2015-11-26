6853b362702d630e36bbba0b41a9153dd7f4b5e6
Stéphane Lenclud <github@lenclud.com>
2015-11-26 19:56:01 +0100

    Adding demo application.

---
+++ NotificationDemo/DemoForm.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace NotificationDemo
+{
+    partial class DemoForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonCreateNotificationIcon = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonCreateNotificationIcon
+            // 
+            this.buttonCreateNotificationIcon.Location = new System.Drawing.Point(81, 74);
+            this.buttonCreateNotificationIcon.Name = "buttonCreateNotificationIcon";
+            this.buttonCreateNotificationIcon.Size = new System.Drawing.Size(144, 23);
+            this.buttonCreateNotificationIcon.TabIndex = 0;
+            this.buttonCreateNotificationIcon.Text = "Create Notification Icon";
+            this.buttonCreateNotificationIcon.UseVisualStyleBackColor = true;
+            this.buttonCreateNotificationIcon.Click += new System.EventHandler(this.buttonCreateNotificationIcon_Click);
+            // 
+            // DemoForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.buttonCreateNotificationIcon);
+            this.Name = "DemoForm";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonCreateNotificationIcon;
+    }
+}
+
---
+++ NotificationDemo/DemoForm.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Reflection;
+
+namespace NotificationDemo
+{
+    public partial class DemoForm : Form
+    {
+
+        /// <summary>
+        /// System tray icon.
+        /// </summary>
+        private SharpLib.Notification.Control iNotifyIcon;
+
+
+        public DemoForm()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonCreateNotificationIcon_Click(object sender, EventArgs e)
+        {
+            SetupTrayIcon();
+        }
+
+
+        /// <summary>
+        ///
+        /// </summary>
+        private void SetupTrayIcon()
+        {
+            iNotifyIcon = new SharpLib.Notification.Control();
+            iNotifyIcon.Icon = GetIcon("notification.ico");
+            iNotifyIcon.Text = "TV Server Status";
+            iNotifyIcon.Visible = true;
+
+            //Double click toggles visibility - typically brings up the application
+
+            /*
+            iNotifyIcon.DoubleClick += delegate (object obj, EventArgs args)
+            {
+                SysTrayHideShow();
+            };
+
+            
+            //Adding a context menu, useful to be able to exit the application
+            ContextMenu contextMenu = new ContextMenu();
+            //Context menu item to toggle visibility
+            MenuItem hideShowItem = new MenuItem("Hide/Show");
+            hideShowItem.Click += delegate (object obj, EventArgs args)
+            {
+                SysTrayHideShow();
+            };
+            contextMenu.MenuItems.Add(hideShowItem);
+
+            //Context menu item separator
+            contextMenu.MenuItems.Add(new MenuItem("-"));
+
+            //Context menu exit item
+            MenuItem exitItem = new MenuItem("Exit");
+            exitItem.Click += delegate (object obj, EventArgs args)
+            {
+                Application.Exit();
+            };
+            contextMenu.MenuItems.Add(exitItem);
+
+            iNotifyIcon.ContextMenu = contextMenu;
+            */
+        }
+
+        /// <summary>
+        /// Access icons from embedded resources.
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        public static Icon GetIcon(string name)
+        {
+            string[] names = Assembly.GetExecutingAssembly().GetManifestResourceNames();
+            for (int i = 0; i < names.Length; i++)
+            {
+                if (names[i].EndsWith(name))
+                {
+                    using (Stream stream = Assembly.GetExecutingAssembly().GetManifestResourceStream(names[i]))
+                    {
+                        return new Icon(stream);
+                    }
+                }
+            }
+
+            return null;
+        }
+
+    }
+}
